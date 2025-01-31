c6e81520198a53c58f52956c35b3772bb0a17be7
Kevin Seghetti <kts@tenetti.org>
2025-01-30 22:35:34 -0800

    chaper 4: authentication done. added user table

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -17,6 +17,10 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Route path="/contact" page={ContactPage} name="contact" />
       <PrivateSet unauthenticated="home">
         <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
